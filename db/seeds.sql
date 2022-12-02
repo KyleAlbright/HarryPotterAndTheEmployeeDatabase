06391035a7888bf07a7f958086a55a84a5703c81
User <kyle_albright@hotmail.com>
2022-12-02 12:55:20 -0600

    added seeds (harry potter themed) added gitignore

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ('Principal'), ('Teacher'), ('Groundskeeper'), ('Janator'), ('Intern'), ('Superintendent');
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+('Headmaster', 150000.00, 2), 
+('Professor', 75000.00, 3), 
+('Keeper of Keys', 55000.00, 4), 
+('Caretaker', 30000.00, 5), 
+('Student Teacher', 20000.00, 6), 
+('Minister of Magic', 400000.00, 1);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES 
+('Albus', 'Dumbledore', 1, 2), 
+('Minerva', 'McGonagall', 2, 3), 
+('Rubeus', 'Hagrid', 3, NULL), 
+('Argus', 'Filch', 4, NULL), 
+('Hermine', 'Granger', 5, NULL), 
+('Cornelius', 'Fudge', 6, 1);
